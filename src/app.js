6b24e335e5b50bcdba6bb4307155c96f73e46027
Tony Khaov <tony.khaov@gmail.com>
2020-10-04 16:21:04 +0200

    Login and go to portal page

--- src/app.js
+++ src/app.js
@@ -9,7 +9,15 @@ const { WEBSITE, ACCESS_CODE, PASSWORD } = process.env;
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto(WEBSITE);
+
+  await page.type("input#login.homeInput", ACCESS_CODE);
+  await page.keyboard.press("Tab");
+  await page.keyboard.type(PASSWORD);
   await page.screenshot({ path: "./dist/screenshots/loginPage.png" });
+
+  await page.click("td.button");
+  await page.waitForNavigation({ waitUntil: "domcontentloaded" });
+  await page.screenshot({ path: "./dist/screenshots/portalPage.png" });
 
   await browser.close();
 })();
