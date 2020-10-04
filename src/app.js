9499d60667f85afa744486e6e9db8437d8208f21
Tony Khaov <tony.khaov@gmail.com>
2020-10-04 18:38:16 +0200

    Improve comments and move expressions to make the code more readable

--- src/app.js
+++ src/app.js
@@ -10,39 +10,39 @@ const { WEBSITE, ACCESS_CODE, PASSWORD, NODE_ENV } = process.env;
 
 (async () => {
   const now = dayjs();
+
   const browser = await puppeteer.launch({ headless: NODE_ENV !== "demo" });
   const page = await browser.newPage();
-  await page.goto(WEBSITE);
+
   await page.setViewport({ width: 640, height: 480, isMobile: true });
+  await page.goto(WEBSITE);
+  await page.screenshot({ path: "./dist/screenshots/loginPage.png" });
+  console.log("--> loginPage");
 
   await page.type("input#login.homeInput", ACCESS_CODE);
   await page.keyboard.press("Tab");
   await page.keyboard.type(PASSWORD);
-  await page.screenshot({ path: "./dist/screenshots/loginPage.png" });
   await page.click("td.button");
-
   await page.waitForSelector("img.clubAvantage");
   await page.screenshot({ path: "./dist/screenshots/portalPage.png" });
-  console.log("loginPage passed");
+  console.log("--> portalPage");
 
   await page.click("img.clubAvantage");
-  console.log("portalPage passed");
-
   await page.setViewport({ width: 900, height: 798, isMobile: false });
-  await page.screenshot({ path: "./dist/screenshots/homePage.png" });
   await page.waitForSelector("#menu_switch");
-  await page.click("#menu_switch");
-  console.log("homePage passed");
+  await page.screenshot({ path: "./dist/screenshots/homePage.png" });
+  console.log("--> homePage");
 
+  await page.click("#menu_switch");
   await page.waitForNavigation({ waitUntil: "domcontentloaded" });
   await page.screenshot({ path: "./dist/screenshots/gestionPage.png" });
-  console.log("gestionPage passed");
+  console.log("--> gestionPage");
 
   await page.click(".dropdown a");
   await page.click(".dropdown-content a");
   await page.waitForSelector("tr.month");
   await page.screenshot({ path: "./dist/screenshots/comissionsPage.png" });
-  console.log("comissionPage passed");
+  console.log("--> comissionPage");
 
   const months = await page.$$eval(".month a[href^='#']", (monthRows) =>
     monthRows.map((month) => month.textContent)
@@ -54,9 +54,7 @@ const { WEBSITE, ACCESS_CODE, PASSWORD, NODE_ENV } = process.env;
   const previousMonthIndex = months.findIndex((month) =>
     month.match(regexPreviousMonth)
   );
-
-  console.log("previousMonth", previousMonth);
-  console.log("previousMonthIndex", previousMonthIndex);
+  console.log("previousMonth:", previousMonth);
 
   await browser.close();
 })();
