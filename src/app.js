ae0cf48869ff9ab04e0147492cd47fc3fe9e4089
Tony Khaov <tony.khaov@gmail.com>
2020-10-04 18:06:45 +0200

    Get months, add missing awaits and add comments

--- src/app.js
+++ src/app.js
@@ -8,8 +8,8 @@ const { WEBSITE, ACCESS_CODE, PASSWORD, NODE_ENV } = process.env;
 (async () => {
   const browser = await puppeteer.launch({ headless: NODE_ENV !== "demo" });
   const page = await browser.newPage();
-  await page.setViewport({ width: 640, height: 480, isMobile: true });
   await page.goto(WEBSITE);
+  await page.setViewport({ width: 640, height: 480, isMobile: true });
 
   await page.type("input#login.homeInput", ACCESS_CODE);
   await page.keyboard.press("Tab");
@@ -19,19 +19,33 @@ const { WEBSITE, ACCESS_CODE, PASSWORD, NODE_ENV } = process.env;
   await page.click("td.button");
   await page.waitForNavigation({ waitUntil: "domcontentloaded" });
   await page.screenshot({ path: "./dist/screenshots/portalPage.png" });
+  console.log("loginPage passed");
 
   await Promise.all([
-    page.waitForSelector("img.clubAvantage"),
+    await page.waitForSelector("img.clubAvantage"),
     await page.click("img.clubAvantage"),
-    page.setViewport({ width: 1366, height: 798, isMobile: false }),
+    await page.setViewport({ width: 900, height: 798, isMobile: false }),
   ]);
-
-  await page.waitForSelector(".dropdown-content");
+  console.log("portalPage passed"),
+    await page.waitForSelector(".dropdown-content");
   await page.screenshot({ path: "./dist/screenshots/homePage.png" });
   await page.click("a#menu_switch");
+  console.log("homePage passed");
 
   await page.waitForNavigation({ waitUntil: "domcontentloaded" });
   await page.screenshot({ path: "./dist/screenshots/gestionPage.png" });
+  console.log("gestionPage passed");
+
+  await page.click(".dropdown a");
+  await page.click(".dropdown-content a");
+  await page.waitForSelector("tr.month");
+  await page.screenshot({ path: "./dist/screenshots/comissionsPage.png" });
+  console.log("comissionPage passed");
+
+  const months = await page.$$eval(".month a[href^='#']", (monthRows) =>
+    monthRows.map((month) => month.textContent)
+  );
+  console.log("months:", months);
 
   await browser.close();
 })();
